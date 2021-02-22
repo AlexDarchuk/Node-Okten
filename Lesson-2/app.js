6acfbcb969870a197a1da7183e25d63409c1bd6d
Your Name <o.darchuk@gmail.com>
2021-02-22 17:06:22 +0200

    add express json

--- Lesson-2/app.js
+++ Lesson-2/app.js
@@ -5,6 +5,7 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('view engine', '.hbs');
