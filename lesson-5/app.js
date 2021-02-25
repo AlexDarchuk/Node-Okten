5d0e26ab5b154f270a4170d9e9478f72e5cd6304
Your Name <o.darchuk@gmail.com>
2021-02-25 22:24:07 +0200

    create lesson-5

---
+++ lesson-5/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const apiRouter = require(null);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', apiRouter);
+
+app.listen(5001, () => {
+    console.log('App listen 5001');
+});
